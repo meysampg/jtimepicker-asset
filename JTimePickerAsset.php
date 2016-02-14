094fe6e819c71718a795bd843051dce864667dff
Meysam <p.g.meysam@gmail.com>
2016-02-14 15:06:04 +0330

    Initial commit

---
+++ JTimePickerAsset.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace meysampg\assets;
+
+use yii\web\AssetBundle;
+
+class JTimePickerAsset extends AssetBundle
+{
+    public $basePath = '@vendor/meysampg/jqueryui-jtimepicker-addon/';
+
+    public $js = [
+        YII_ENV_DEV ? 'jquery-ui-timepicker-cc.js' : 'jquery-ui-timepicker-cc.min.js',
+        YII_ENV_DEV ? 'jquery-ui-timepicker-addon.js' : 'jquery-ui-timepicker-addon.min.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\jui\JuiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+    ];
+}
